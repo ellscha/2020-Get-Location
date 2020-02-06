eef82fc54fbac4558463d6185145222da67255a4
ellscha <escharlin@wesleyan.edu>
2020-02-05 21:59:18 -0500

    location manager working

--- DisplayCurrentLocation/ViewController.swift
+++ DisplayCurrentLocation/ViewController.swift
@@ -14,11 +14,20 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
 
     let locationManager = CLLocationManager()
     override func viewDidLoad() {
-        locationManager.requestAlwaysAuthorization()
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        locationManager.requestAlwaysAuthorization()
+        locationManager.requestWhenInUseAuthorization()
+        self.setupLocationManager()
+        }
+    
+    func setupLocationManager() {
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyBestForNavigation
+            locationManager.startUpdatingLocation()
+        }
     }
-
+    
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         guard let locValue: CLLocationCoordinate2D = manager.location?.coordinate else { return }
         print("locations = \(locValue.latitude), \(locValue.longitude)")
